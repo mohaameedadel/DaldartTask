f47ba7c91b9d00de6b1a22d470cbbd4e8ac8bfd3
mohaameedadel <doooooolaaaaaa@gmail.com>
2024-09-23 23:43:35 +0300

    update

--- src/Pages/NotFoundPages/NotFoundPages.tsx
+++ src/Pages/NotFoundPages/NotFoundPages.tsx
@@ -1,6 +1,11 @@
+import error from "../../assets/404.svg"
 export default function NotFoundPages() {
   return (
-    <div>NotFoundPages</div>
-  )
+    <>
+      <div className="container">
+        <img src={error} className="max-w-full mx-auto" alt="404 error" />
+        <h2 className="text-3xl text-main text-center font-bold uppercase my-2">Page NotFound</h2>
+      </div>
+    </>
+  );
 }
-
